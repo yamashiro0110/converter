775c26d0691e9f32aef2993795d7c2da3c9f6e9f
yamashiro-r <yamashiro0110@gmail.com>
2014-03-21 13:46:02 +0900

    引数で渡された文字列をconvertするクラス

---
+++ src/conv/combined.py
@@ -0,0 +1,45 @@
+# coding : UTF-8
+'''
+Created on 2014/03/21
+@author: yamashiro-r
+'''
+import re
+
+# ******************************************
+class Combined(object):
+    """
+    Combined is Apache Log Format
+        LogFormat "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\" %D" combined
+    @date 2014/03/21
+    """
+
+    def __init__(self):
+        print "Combined init"
+        self.name = "Combined"
+
+    def getConvertText(self, line):
+        pattern = re.compile(
+                 r'^([0-9\.]+?) (.+?) (.+?) \[(.+?) \+[0-9]+\] \"(.+?) (.+?) (.+?)\" ([0-9]+?) ([0-9]+?) \"(.+?)\" \"(.+?)\"'
+            )
+
+        match = pattern.match(line)
+
+        if match:
+            csv = "\"{arg1}\",\"{arg2}\",\"{arg3}\",\"{arg4}\",\"{arg5}\",\"{arg6}\",\"{arg7}\",\"{arg8}\",\"{arg9}\",\"{arg10}\",\"{arg11}\"\n"
+            return csv.format(
+                arg1 = match.group(1),
+                arg2 = match.group(2),
+                arg3 = match.group(3),
+                arg4 = match.group(4),
+                arg5 = match.group(5),
+                arg6 = match.group(6),
+                arg7 = match.group(7),
+                arg8 = match.group(8),
+                arg9 = match.group(9),
+                arg10 = match.group(10),
+                arg11 = match.group(11)
+            )
+
+        else:
+            return ""
+
---
+++ src/conv/dummy.py
@@ -0,0 +1,16 @@
+'''
+Created on 2014/03/21
+@author: yamashiro-r
+'''
+
+class Dummy(object):
+    """
+    This Class iS Convert Dummy
+    """
+
+    def __init__(self):
+        self.name = "Dummy"
+        print "init Dummy"
+
+    def getConvertText(self, line):
+        return "Dummy getConvertText"
